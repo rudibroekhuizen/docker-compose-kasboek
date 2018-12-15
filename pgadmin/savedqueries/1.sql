f6008cf7d5b4efcbaad375bd6f3bd6fad2a5702c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-12-15 19:49:10 +0100

    Update 1.sql

--- pgadmin/savedqueries/1.sql
+++ pgadmin/savedqueries/1.sql
@@ -1,3 +1,12 @@
 SET LC_MONETARY="nl_NL.utf8";
 
 CREATE SCHEMA kasboek;
+
+CREATE DATABASE kasboek
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'nl_NL.utf8'
+    LC_CTYPE = 'nl_NL.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
