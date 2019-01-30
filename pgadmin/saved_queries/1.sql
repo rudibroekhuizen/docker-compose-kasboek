f4aeb7d3ae0bf34901961ce437a87f5dd52c10d9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-01-30 16:30:59 +0100

    Update 1.sql

--- pgadmin/saved_queries/1.sql
+++ pgadmin/saved_queries/1.sql
@@ -13,6 +13,19 @@ CREATE SCHEMA kasboek;
 
 SET search_path TO kasboek;
 
+CREATE TABLE transacties
+(
+    datum timestamp without time zone,
+    naam character varying COLLATE pg_catalog."default",
+    rekening character varying COLLATE pg_catalog."default",
+    tegenrekening character varying COLLATE pg_catalog."default",
+    code character varying COLLATE pg_catalog."default",
+    af_bij character varying COLLATE pg_catalog."default",
+    bedrag money,
+    mutatiesoort character varying COLLATE pg_catalog."default",
+    mededeling character varying COLLATE pg_catalog."default"
+);
+
 SELECT account_iban, description
 FROM transacties t1
 LEFT JOIN spaarrekeningen t2 ON t1.opposing_iban = t2.iban
