12766c9506d2862b56f142796fcd69b023dcf5ad
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-01-30 17:20:37 +0100

    Update 1.sql

--- pgadmin/saved_queries/1.sql
+++ pgadmin/saved_queries/1.sql
@@ -29,7 +29,8 @@ CREATE TABLE kasboek.transacties
 
 UPDATE kasboek.transacties SET tsvector = to_tsvector('simple', COALESCE(transacties.naam) || ' ' || COALESCE(transacties.rekening) || ' ' || COALESCE(transacties.tegenrekening) || ' ' || COALESCE(transacties.code) || ' ' || COALESCE(transacties.af_bij) || ' ' || COALESCE(transacties.mutatiesoort) || ' ' || COALESCE(transacties.mededeling));
 
-SELECT t1.rekening, t1.tegenrekening, t1.mededeling
-FROM transacties t1
-LEFT JOIN spaarrekeningen t2 ON t1.tegenrekening = t2.rekening
-WHERE t2.rekening IS null;
+SELECT t1.rekening, t1.tegenrekening, t1.naam, t1.mededeling
+FROM kasboek.transacties t1
+LEFT JOIN kasboek.spaarrekeningen t2 ON t1.tegenrekening = t2.rekening
+WHERE t2.rekening IS null
+AND tsvector @@ websearch_to_tsquery('simple','ikea');
