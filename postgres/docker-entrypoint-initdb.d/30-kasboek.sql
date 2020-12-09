41cf42249c68ef25c523d362b0ab473b5aeb598f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-09 22:07:15 +0100

    Update 30-kasboek.sql

--- postgres/docker-entrypoint-initdb.d/30-kasboek.sql
+++ postgres/docker-entrypoint-initdb.d/30-kasboek.sql
@@ -1,16 +1,3 @@
-SET LC_MONETARY="nl_NL.utf8";
-
-CREATE DATABASE kasboek
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'nl_NL.utf8'
-    LC_CTYPE = 'nl_NL.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
-CREATE SCHEMA kasboek;
-
 CREATE TABLE kasboek.transacties
   (
     id INT GENERATED ALWAYS AS IDENTITY,
