987a4407daf97658bc179bfed19663d3425e7174
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-10 11:38:26 +0100

    Update 20-kasboek.sh

--- postgres/docker-entrypoint-initdb.d/20-kasboek.sh
+++ postgres/docker-entrypoint-initdb.d/20-kasboek.sh
@@ -15,5 +15,43 @@ EOSQL
 
 psql --dbname=kasboek -v ON_ERROR_STOP=1 --username="$POSTGRES_USER" <<-EOSQL
   CREATE EXTENSION pg_trgm;
+  
   CREATE SCHEMA kasboek;
+  
+  CREATE TABLE kasboek.transacties
+  (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    datum timestamp without time zone,
+    naam character varying COLLATE pg_catalog."default",
+    rekening character varying COLLATE pg_catalog."default",
+    tegenrekening character varying COLLATE pg_catalog."default",
+    code character varying COLLATE pg_catalog."default",
+    af_bij character varying COLLATE pg_catalog."default",
+    bedrag money,
+    mutatiesoort character varying COLLATE pg_catalog."default",
+    mededeling character varying COLLATE pg_catalog."default",
+    tsvector tsvector
+  );
+  
+  CREATE TRIGGER create_tsv BEFORE INSERT OR UPDATE
+  ON kasboek.transacties FOR EACH ROW EXECUTE FUNCTION
+  tsvector_update_trigger(tsvector, 'pg_catalog.simple', 
+  naam,
+  rekening,
+  tegenrekening,
+  mededeling
+  );
+  
+  COPY kasboek.transacties (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) 
+  FROM '/mnt/miniodata/bucket/transacties.csv' csv header;
+
+  CREATE TABLE kasboek.words AS SELECT * FROM ts_stat('SELECT tsvector FROM kasboek.transacties');
+
+  CREATE INDEX ON kasboek.words USING gin (word gin_trgm_ops);
+
+  CREATE TABLE IF NOT EXISTS kasboek.mijn_rekeningen (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  rekening text
+  );
+  
 EOSQL
