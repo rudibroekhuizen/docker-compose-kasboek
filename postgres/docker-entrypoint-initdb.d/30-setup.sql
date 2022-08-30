807f00662779b7f849e23afa9a222ba95c8ed679
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2022-08-30 23:56:55 +0200

    Update 30-setup.sql

--- postgres/docker-entrypoint-initdb.d/30-setup.sql
+++ postgres/docker-entrypoint-initdb.d/30-setup.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE PROCEDURE copy_from(
+CREATE OR REPLACE PROCEDURE copy_from_ing(
    _source_file text
 ) AS
 $$
 BEGIN 
-EXECUTE format('COPY kasboek.transacties (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
+EXECUTE format('COPY kasboek.transacties_ing (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
 END; 
 $$ LANGUAGE plpgsql;
 
--- CALL copy_from('/mnt/miniodata/kasboek/transacties.csv');
+-- CALL copy_from('/mnt/miniodata/kasboek/transacties_ing.csv');
