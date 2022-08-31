edf6c93b3537c476ac668cba7aa57552adde8262
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2022-08-31 07:47:53 +0200

    Add commit

--- postgres/docker-entrypoint-initdb.d/30-setup.sql
+++ postgres/docker-entrypoint-initdb.d/30-setup.sql
@@ -3,11 +3,12 @@ CREATE OR REPLACE PROCEDURE kasboek.copy_from_ing(
    _source_file text
 ) AS
 $$
-BEGIN 
-EXECUTE format('COPY kasboek.transacties_ing (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
-CREATE TABLE kasboek.words_ing AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_ing');
-CREATE INDEX ON kasboek.words_ing USING gin (word gin_trgm_ops);
-END; 
+BEGIN
+  EXECUTE format('COPY kasboek.transacties_ing (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
+  CREATE TABLE kasboek.words_ing AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_ing');
+  CREATE INDEX ON kasboek.words_ing USING gin (word gin_trgm_ops);
+  COMMIT;
+END;
 $$ LANGUAGE plpgsql;
 
 -- CALL copy_from_ing('/mnt/miniodata/kasboek/transacties_ing.csv');
@@ -18,11 +19,12 @@ CREATE OR REPLACE PROCEDURE kasboek.copy_from_asn(
    _source_file text
 ) AS
 $$
-BEGIN 
-EXECUTE format('COPY kasboek.transacties_asn (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
-CREATE TABLE kasboek.words_asn AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_asn');
-CREATE INDEX ON kasboek.words_asn USING gin (word gin_trgm_ops);
-END; 
+BEGIN
+  EXECUTE format('COPY kasboek.transacties_asn (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
+  CREATE TABLE kasboek.words_asn AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_asn');
+  CREATE INDEX ON kasboek.words_asn USING gin (word gin_trgm_ops);
+  COMMIT;
+END;
 $$ LANGUAGE plpgsql;
 
 -- CALL copy_from_asn('/mnt/miniodata/kasboek/transacties_asn.csv');
