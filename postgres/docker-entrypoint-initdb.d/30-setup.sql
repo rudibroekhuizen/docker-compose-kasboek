acd66f77cee42d73685484e1f64de39102f877f2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2022-08-31 07:25:21 +0200

    Update 30-setup.sql

--- postgres/docker-entrypoint-initdb.d/30-setup.sql
+++ postgres/docker-entrypoint-initdb.d/30-setup.sql
@@ -1,10 +1,28 @@
+-- Insert ING transactions
 CREATE OR REPLACE PROCEDURE copy_from_ing(
    _source_file text
 ) AS
 $$
 BEGIN 
 EXECUTE format('COPY kasboek.transacties_ing (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
+CREATE TABLE kasboek.words_ing AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_ing');
+CREATE INDEX ON kasboek.words_ing USING gin (word gin_trgm_ops);
 END; 
 $$ LANGUAGE plpgsql;
 
--- CALL copy_from('/mnt/miniodata/kasboek/transacties_ing.csv');
+-- CALL copy_from_ing('/mnt/miniodata/kasboek/transacties_ing.csv');
+
+
+-- Insert ASN transactions
+CREATE OR REPLACE PROCEDURE copy_from_asn(
+   _source_file text
+) AS
+$$
+BEGIN 
+EXECUTE format('COPY kasboek.transacties_asn (datum, naam, rekening, tegenrekening, code, af_bij, bedrag, mutatiesoort, mededeling) FROM %L csv header', _source_file);
+CREATE TABLE kasboek.words_asn AS SELECT * FROM ts_stat('SELECT tsv FROM kasboek.transacties_asn');
+CREATE INDEX ON kasboek.words_asn USING gin (word gin_trgm_ops);
+END; 
+$$ LANGUAGE plpgsql;
+
+-- CALL copy_from_asn('/mnt/miniodata/kasboek/transacties_asn.csv');
