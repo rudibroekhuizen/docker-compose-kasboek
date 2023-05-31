3e2f6bf391e5d8945af30af1b13ff356bfb9e394
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2023-06-01 00:30:39 +0200

    On MD5 ROW

--- postgres/docker-entrypoint-initdb.d/30-setup.sql
+++ postgres/docker-entrypoint-initdb.d/30-setup.sql
@@ -21,7 +21,7 @@ BEGIN
     ) FROM %L csv header', _source_file);
   MERGE INTO transacties_ing ti
   USING transacties_ing_raw tir
-  ON (ti.md5_hash = tir.md5_hash)
+  ON MD5(ROW(ti.boekingsdatum, ti.tegenrekeningnummer, ti.naam_tegenrekening, ti.transactiebedrag, ti.journaaldatum, ti.volgnummer_transactie, ti.omschrijving)::text) = MD5(ROW(tir.boekingsdatum, tir.tegenrekeningnummer, tir.naam_tegenrekening, tir.transactiebedrag, tir.journaaldatum, tir.volgnummer_transactie, tir.omschrijving)::text)
   WHEN MATCHED THEN
   UPDATE SET
     datum = tir.datum,
